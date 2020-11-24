6a01300f7339e0b9c33d21773f39eb7a75495115
Claudio Marchini <claudiomarchini2003@outlook.com>
2020-11-24 17:44:11 +0100

    Aggiornato Regioni

---
+++ Informatica/Marchini.Claudio.Regioni/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Marchini.Claudio.Regioni
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, List<Provincia>> provincie = new Dictionary<string, List<Provincia>>();
+        public MainWindow()
+        {
+            CaricaProvincie();
+            InitializeComponent();
+        }
+
+        private void CaricaProvincie()
+        {
+            using (var sr = new StreamReader("Dati.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] dati = sr.ReadLine().Trim().Split(',');
+                    var provincia = new Provincia(dati[1], dati[0]);
+                    if (!provincie.ContainsKey(dati[2]))
+                        provincie.Add(dati[2], new List<Provincia>());
+                    provincie[dati[2]].Add(provincia);
+                }
+            }
+        }
+
+        private void OncmbRegioniSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var tmp = sender as ComboBox;
+            dtgProvince.ItemsSource = provincie[((ComboBoxItem)tmp.SelectedItem).Content.ToString()];      
+        }
+    }
+}
---
+++ Informatica/Marchini.Claudio.Regioni/Provincia.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Marchini.Claudio.Regioni
+{
+    public class Provincia
+    {
+        public string Nome { get; private set; }
+        public string Sigla { get; private set; }
+
+        public Provincia(string nome, string sigla)
+        {
+            Nome = nome;
+            Sigla = sigla;
+        }
+
+        public static List<Provincia> operator +(List<Provincia> sinistra, Provincia destra)
+        {
+            sinistra.Add(destra);
+            return sinistra;
+        }
+    }
+}
