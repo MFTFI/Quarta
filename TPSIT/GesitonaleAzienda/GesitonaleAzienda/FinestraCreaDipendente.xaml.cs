dd64bba16401aa67c0ca60359188d0cc79346088
Claudio Marchini <claudiomarchini2003@gmail.com>
2021-01-24 19:57:57 +0100

    Finito programma "Gestionale azienda"

--- TPSIT/GesitonaleAzienda/GesitonaleAzienda/FinestraCreaDipendente.xaml.cs
+++ TPSIT/GesitonaleAzienda/GesitonaleAzienda/FinestraCreaDipendente.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,36 @@ namespace GesitonaleAzienda
     /// </summary>
     public partial class FinestraCreaDipendente : Window
     {
+        public static event EventHandler<Dipendente> OnFinestraDipendenteBtnCreaClick;
+
         public FinestraCreaDipendente()
         {
             InitializeComponent();
+            for (int i = 1; i < 32; i++)
+            {
+                CmbGiornoNascita.Items.Add(i);
+            }
+            for (int i = DateTime.Today.Year; i > 1900; i--)
+            {
+                CmbAnnoNascita.Items.Add(i);
+            }
+            foreach (var mese in new DateTimeFormatInfo().MonthNames)
+            {
+                CmbMeseNascita.Items.Add(mese);
+            }
+        }
+
+        private void BtnCreaDipendenteClick(object sender, RoutedEventArgs e)
+        {
+            string nome = TbxNome.Text;
+            string cognome = TbxCognome.Text;
+            string luogoDiNascita = TbxLuogoNascita.Text;
+            int anno = (int)CmbAnnoNascita.SelectedItem;
+            int mese = CmbMeseNascita.SelectedIndex + 1;
+            int giorno = (int)CmbGiornoNascita.SelectedItem;
+            DateTime giornoDiNascita = new DateTime(anno, mese, giorno);
+            Dipendente dipendente = new Dipendente(nome, cognome, luogoDiNascita, giornoDiNascita);
+            OnFinestraDipendenteBtnCreaClick.Invoke(this, dipendente);
         }
     }
 }
--- TPSIT/GesitonaleAzienda/GesitonaleAzienda/MainWindow.xaml.cs
+++ TPSIT/GesitonaleAzienda/GesitonaleAzienda/MainWindow.xaml.cs
@@ -20,9 +20,16 @@ namespace GesitonaleAzienda
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Contratto> contratti = new List<Contratto>();
+        List<Dipendente> dipendenti = new List<Dipendente>() /*{ new Dipendente("mario", "rossi", "rimini", DateTime.Now), new Dipendente("luigi", "rossi", "rimini", DateTime.Now) }*/;
+
         public MainWindow()
         {
             InitializeComponent();
+            FinestraCreaAttivita.OnFinestraAttivitaBtnCreaClick += OttieniContratto;
+            FinestraCreaDipendente.OnFinestraDipendenteBtnCreaClick += OttieniDipendente;
+            DtgContratti.ItemsSource = contratti;
+            DtgDipendenti.ItemsSource = dipendenti;
         }
 
         private void BtnAggiungiDipendenteClick(object sender, RoutedEventArgs e)
@@ -37,6 +44,59 @@ namespace GesitonaleAzienda
             FinestraCreaAttivita window = new FinestraCreaAttivita();
             window.Owner = this;
             window.ShowDialog();
+        }
+
+        private void OttieniContratto(object sender, Contratto e)
+        {
+            if (e != null)
+            {
+                if (DtgDipendenti.SelectedItem != null)
+                    (DtgDipendenti.SelectedItem as Dipendente).Contratti.Add(e);
+                else
+                    MessageBox.Show("Selezionare nella combo box un dipendente a cui aggiungere un attivita");
+            }
+            DtgContratti.Items.Refresh();
+        }
+
+        private void OttieniDipendente(object sender, Dipendente e)
+        {
+            if (e != null)
+                dipendenti.Add(e);
+            DtgDipendenti.Items.Refresh();
+        }
+
+        private void CmbAttivitaModificaClick(object sender, RoutedEventArgs e)
+        {
+            FinestraCreaAttivita window = new FinestraCreaAttivita();
+            window.Owner = this;
+            window.Show();
+        }
+
+        private void CmbDipendenteModificaClick(object sender, RoutedEventArgs e)
+        {
+            FinestraCreaAttivita window = new FinestraCreaAttivita();
+            window.Owner = this;
+            window.Show();
+        }
+
+        private void CmbAttivitaEliminaClick(object sender, RoutedEventArgs e)
+        {
+            if (DtgContratti.SelectedIndex != -1)
+                contratti.RemoveAt(DtgContratti.SelectedIndex);
+            DtgContratti.Items.Refresh();
+        }
+
+        private void CmbDipendenteEliminaClick(object sender, RoutedEventArgs e)
+        {
+            if (DtgDipendenti.SelectedIndex != -1)
+                dipendenti.RemoveAt(DtgDipendenti.SelectedIndex);
+            DtgDipendenti.Items.Refresh();
+        }
+
+        private void DtgDipendentiSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(e.RemovedItems.Count == 0)
+                DtgContratti.ItemsSource = (DtgDipendenti.SelectedItem as Dipendente).Contratti;
         }
     }
 }
