73b879651e3790dc38fd67604495ffe684e444e6
SignedSnow0 <claudiomarchini2003@gmail.com>
2021-02-21 12:14:27 +0100

    Added Esercitazioni form

---
+++ TPSIT/EsercitazioniForm/contattaci.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Contattaci</title>
+</head>
+</html>
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['nome'];
+$testo = $_POST['nome'];
+$trattamentoDati = $_POST['trattamentoDati'];
+$newsletter = $_POST['newsletter'];
+var_dump($_POST);
---
+++ TPSIT/EsercitazioniForm/nuovaRicetta.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Nuova ricetta</title>
+</head>
+</html>
+<?php
+$titolo = $_POST['titolo'];
+$descrizione = $_POST['descrizione'];
+$ingredienti = $_POST['ingredienti'];
+$passaggi = $_POST['procedimento'];
+var_dump($_POST);
