af9c51322cb7daf579cb15a5f0b2455a914ac3e8
SignedSnow0 <claudiomarchini2003@gmail.com>
2021-02-28 16:03:35 +0100

    Aggiunte cose

---
+++ Informatica/Infissa-Postfissa/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infissa_Postfissa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prova = "(3+2)*4";
+            string prova2 = "(2+1)*(3-2)";
+            string prova3 = "1*(7-4)";
+
+            Console.WriteLine(CreaPostfissa(prova)); 
+            Console.WriteLine(CreaPostfissa(prova2));
+            Console.WriteLine(CreaPostfissa(prova3));
+
+            Console.ReadKey();
+        }
+
+        static string CreaPostfissa(string infissa)
+        {
+            string solution = "";
+            StringBuilder ss = new StringBuilder();
+            List<char> caratteri = new List<char>();
+            foreach (var carattere in infissa)
+                caratteri.Add(carattere);
+            
+            for (int i = 0; i < caratteri.Count; i++)
+            {
+                if (caratteri[i] == '(' && caratteri[i + 1] != '(')
+                {
+                    ss = new StringBuilder();
+                    ss.Append(caratteri[i + 1]);
+                    ss.Append(caratteri[i + 3]);
+                    ss.Append(caratteri[i + 2]);
+                    solution += ss;
+
+                    for (int j = 0; j < 5; j++)
+                        caratteri.RemoveAt(i);
+
+                    i = 0;
+                }
+            }
+
+            if (caratteri.Count > 1)
+            {
+                if (caratteri[0] == '+' || caratteri[0] == '-' || caratteri[0] == '*' || caratteri[0] == '/')
+                {
+                    ss = new StringBuilder();
+                    ss.Append(solution);
+                    ss.Append(caratteri[1]);
+                    ss.Append(caratteri[0]);
+                    solution = "";
+                    solution += ss;
+                }
+                else
+                {
+                    ss = new StringBuilder();
+                    ss.Append(caratteri[0]);
+                    ss.Append(solution);
+                    ss.Append(caratteri[1]);
+                    solution = "";
+                    solution += ss;
+                }
+            }
+            else
+            {
+                ss = new StringBuilder();
+                ss.Append(solution);
+                ss.Append(caratteri[0]);
+                solution = "";
+                solution += ss;
+            }
+            return solution;
+        }
+    }
+}
--- TPSIT/BlackFriday/index.php
+++ TPSIT/BlackFriday/index.php
@@ -1,12 +1,42 @@
 <?php
+$prodotti = array();
+$carrello = array();
 include "header.html";
 
+CreaCarrello();
 include "Prodotto.php";
-$prodotto = new Prodotto("Telefono", "50", "resources/telefono.png");
-$immagine = $prodotto->GetImmagine();
-$nome = $prodotto->GetNome();
-$prezzo = $prodotto->GetPrezzo();
-echo "<label>$nome |</label><label> Costo: $prezzo</label><br>";
-echo "<img src=$immagine/><br>";
+$telefono = new Prodotto("Telefono", "50", "resources/telefono.jpg");
+$scatola = new Prodotto("Scatola", "43", "resources/scatola.png");
+$libro = new Prodotto("Libro", "58", "resources/libro.jpg");
+$maschera = new Prodotto("Maschera Pagliaccio", "32", "resources/pagliaccio.jpg");
+array_push($prodotti, $telefono);
+array_push($prodotti, $scatola);
+array_push($prodotti, $libro);
+array_push($prodotti, $maschera);
+RiempiProdotti($prodotti);
 
 include "footer.html";
+
+function RiempiProdotti($prodotti)
+{
+    foreach($prodotti as $prodotto) {
+        $nome = $prodotto->GetNome();
+        $prezzo = $prodotto->GetPrezzo();
+        $immagine = $prodotto->GetImmagine();
+        echo "<div>";
+        echo "<label>$nome |</label><label> Costo: $prezzo euro</label><br>";
+        echo "<img style=' width: 300px; height: 300px;' src=$immagine/><br>";
+        echo "<button>Aggiungi al carrello</button>";
+        echo "</div>";
+    }
+}
+
+function CreaCarrello()
+{
+    echo "<div style=\"height: 120px; width: 9000px; background-color: grey;\">";
+    echo   "<button>
+            <img src='resources/carrello.png' width='100px' height='70px'/><br>
+            Il tuo carrello
+            </button>";
+    echo "</div>";
+}
